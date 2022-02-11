5c3a027f95b5272037f8b2b73a4fe10b6938a916
Matthias Seeger <matthis@amazon.de>
2022-02-11 09:07:32 +0100

    Make BB surrogates work with serialization, change config_space of nashpobench (log spacing, or finrange). Also, config_space for surrogate)

--- benchmarking/definitions/definition_nashpobench.py
+++ benchmarking/definitions/definition_nashpobench.py
@@ -10,25 +10,37 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
+from syne_tune.search_space import choice, uniform, loguniform, lograndint, \
+    logfinrange, finrange
 
-from sklearn.ensemble import RandomForestRegressor
-
-from syne_tune.search_space import choice, randint, uniform, loguniform
-
-from benchmarking.blackbox_repository.conversion_scripts.scripts.fcnet_import import \
-    METRIC_ELAPSED_TIME, METRIC_VALID_LOSS, RESOURCE_ATTR, BLACKBOX_NAME
+from benchmarking.blackbox_repository.conversion_scripts.scripts.fcnet_import \
+    import METRIC_ELAPSED_TIME, METRIC_VALID_LOSS, RESOURCE_ATTR, \
+    BLACKBOX_NAME
 
 
-_config_space = {
+__config_space = {
     "hp_activation_fn_1": choice(["tanh", "relu"]),
     "hp_activation_fn_2": choice(["tanh", "relu"]),
-    "hp_batch_size": randint(8, 64),
-    "hp_dropout_1": uniform(0.0, 0.6),
-    "hp_dropout_2": uniform(0.0, 0.6),
-    "hp_init_lr": loguniform(0.0005, 0.1),
     'hp_lr_schedule': choice(["cosine", "const"]),
-    'hp_n_units_1': randint(16, 512),
-    'hp_n_units_2': randint(16, 512),
+}
+
+_config_space = {
+    True: dict(
+        __config_space,
+        hp_batch_size=lograndint(8, 64),
+        hp_dropout_1=uniform(0.0, 0.6),
+        hp_dropout_2=uniform(0.0, 0.6),
+        hp_init_lr=loguniform(0.0005, 0.1),
+        hp_n_units_1=lograndint(16, 512),
+        hp_n_units_2=lograndint(16, 512)),
+    False: dict(
+        __config_space,
+        hp_batch_size=logfinrange(8, 64, 4),
+        hp_dropout_1=finrange(0.0, 0.6, 3),
+        hp_dropout_2=finrange(0.0, 0.6, 3),
+        hp_init_lr=choice([0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]),
+        hp_n_units_1=logfinrange(16, 512, 6),
+        hp_n_units_2=logfinrange(16, 512, 6))
 }
 
 
@@ -45,6 +57,7 @@ def nashpobench_default_params(params=None):
         'framework_version': '1.6',
         'dataset_name': 'protein_structure',
         'dont_sleep': dont_sleep,
+        'interpolate_blackbox': True,
     }
 
 
@@ -53,13 +66,23 @@ def nashpobench_benchmark(params):
     The underlying tabulated blackbox does not have an `elapsed_time_attr`,
     but only a `time_this_resource_attr`.
 
+    The boolean parameter `interpolate_blackbox` decides whether the
+    tabulated blackbox values are interpolated (using a random forest), in
+    which case the hyperparameter ranges are intervals, or whether the
+    ranges are only exactly covering the tabulated grid.
+    Note that the latter leads to a larger encoded dimension (which can
+    be a problem for Bayesian optimization), because one of the numerical
+    parameters has to be encoded as categorical.
+
     """
+    interpolate_blackbox = params['interpolate_blackbox']
     config_space = dict(
-        _config_space,
+        _config_space[interpolate_blackbox],
         epochs=params['max_resource_level'],
         dataset_name=params['dataset_name'],
         dont_sleep=params['dont_sleep'],
         blackbox_repo_s3_root=params.get('blackbox_repo_s3_root'))
+    surrogate = 'random_forest' if interpolate_blackbox else None
     return {
         'script': None,
         'metric': METRIC_VALID_LOSS,
@@ -71,7 +94,6 @@ def nashpobench_benchmark(params):
         'cost_model': None,
         'supports_simulated': True,
         'blackbox_name': BLACKBOX_NAME,
-        'surrogate': RandomForestRegressor(max_samples=0.005, bootstrap=True),
+        'surrogate': surrogate,
         'time_this_resource_attr': METRIC_ELAPSED_TIME,
     }
-
